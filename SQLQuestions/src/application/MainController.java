cc6f29d8b0db00e7aed6efd68448cf318183f2b5
Go Komura <gomurin0428@gmail.com>
2021-11-24 22:25:26 +0900

    とりあえずWebViewを動かせるようになった

--- SQLQuestions/src/application/MainController.java
+++ SQLQuestions/src/application/MainController.java
@@ -2,12 +2,19 @@ package application;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
 
 public class MainController {
+	
+	@FXML private WebView webView;
+	
+	@FXML private Button button;
 
 	public void handle(Event arg0) {
 		
-		System.out.println("hello");
+		webView.getEngine().load("https://www.google.co.jp/");
 		
 	}
 }
