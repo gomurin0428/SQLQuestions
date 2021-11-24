6e01d8a8b8ad32debaf3d1db3853dd5da3ad950b
Go Komura <gomurin0428@gmail.com>
2021-11-24 21:53:29 +0900

    aaa

---
+++ SQLQuestions/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			SplitPane fxml;
+			fxml =FXMLLoader.load(getClass().getResource("test.fxml"));
+			Scene scene = new Scene(fxml);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ SQLQuestions/src/application/MainController.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class MainController {
+
+	public void handle(Event arg0) {
+		
+		System.out.println("hello");
+		
+	}
+}
